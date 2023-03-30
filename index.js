25eb996a3b45d4e3c06671c849af410becbe0ec6
Saint_movicx <victoride013@gmail.com>
2023-03-30 22:03:34 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+//creating a function for the toggle button to show a div or navnode
+function show() {
+    //call the (ID).style.display ="block";
+    document.getElementById('id0l').style.display = 'block';
+}
